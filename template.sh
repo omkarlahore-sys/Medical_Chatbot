cd74c24b0ecbe808a5c2753949b250be133062e8
omkarlahore-sys <omkarlahore2299@gmail.com>
2025-11-18 13:38:51 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/_init_.py
+touch src/promt.py
+touch src/helper.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo " files created successfully"
